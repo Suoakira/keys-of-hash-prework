436da8b10955b0873ca6831a6665bc8e9658db4a
Stephen Courtier <steves0089@gmail.com>
2018-09-14 10:09:43 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,7 @@
 class Hash
   def keys_of(arguments)
     animal = {
-    []
+    :Panama => []
     ["red-footed tortoise"]
     ["aye-aye", "tomato frog"]
     ["sugar glider", "kangaroo", "koala"]
